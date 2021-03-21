51419967d7a33ad175bb66427cc74be5f23de5a2
KAJIWARA Yutaka <20511091yk@grad.tama.ac.jp>
2021-03-22 02:31:26 +0900

    画像はバックエンド側から取得するように変更

--- src/ChoiceButton.tsx
+++ src/ChoiceButton.tsx
@@ -15,7 +15,8 @@ class ChoiceButton extends React.Component<ChoiceButtonProps> {
     const backgroundStyle:{ [key: string]: string } ={
       background: this.props.pokemonChoice.color
     }
-    const imgPath = `../pokemon${this.props.pokemonChoice.index + 1 }.png?${this.props.pokemonChoice.hash}`;
+    const hostname = window.location.hostname
+    const imgPath = `http://${hostname}:3000/pokemon/${this.props.pokemonChoice.no}.png?${this.props.pokemonChoice.hash}`;
     return (
       <div style={backgroundStyle}>
         <input type="image" src={imgPath} alt="送信する"  onClick={n => this.onPressImg(this.props.pokemonChoice)}/>
